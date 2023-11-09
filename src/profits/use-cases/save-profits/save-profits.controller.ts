4cf74128e876731e4277d43b5129bde4d861fb31
João Henrique <henriquecontact@gmail.com>
2023-11-09 11:15:05 -0300

    Create save-profits.controller

---
+++ src/profits/use-cases/save-profits/save-profits.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { ProfitsRepository } from "../../repositories/profits.repository";
+
+
+@Controller('/profits')
+export class SaveProfitsController {
+
+  constructor (private profitsRepository: ProfitsRepository) {};
+  
+  @Post('save')
+  insertProfit(@Body() profitValue){
+    console.log(profitValue)
+    this.profitsRepository.salvar(profitValue.Value);
+    console.log(profitValue.Value);
+    return profitValue.Value;
+    }
+  }
