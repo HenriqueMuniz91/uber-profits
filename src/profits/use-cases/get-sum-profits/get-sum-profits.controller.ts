8ef1674908a63f8fade6d7a8446e6a9f4847665f
João Henrique <henriquecontact@gmail.com>
2023-11-17 12:07:47 -0300

    Criate Get Sum

---
+++ src/profits/use-cases/get-sum-profits/get-sum-profits.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from "@nestjs/common";
+import { ProfitsRepository } from "src/profits/repositories/profits.repository";
+
+@Controller('/profits')
+export class GetSumProfitsController {
+
+  constructor(private profitsRepository: ProfitsRepository) {};
+
+  @Get('sum')
+  getSumProfits() {
+    const profits = this.profitsRepository.get();
+    const sum = profits.reduce((accumulator,value) => accumulator + value, 0);
+    return sum;
+  };
+};
