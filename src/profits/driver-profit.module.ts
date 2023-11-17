5daa7245ae62c2e7c658fe867b686020b1c7fb61
João Henrique <henriquecontact@gmail.com>
2023-11-17 12:05:36 -0300

    Inclusão de Get Sum

--- src/profits/driver-profit.module.ts
+++ src/profits/driver-profit.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { SaveProfitsController } from './use-cases/save-profits/save-profits.controller';
 import { GetProfitsController } from './use-cases/get-profits/get-profits.controller';
 import { ProfitsRepository } from './repositories/profits.repository';
+import { GetSumProfitsController } from './use-cases/get-sum-profits/get-sum-profits.controller';
 
 @Module({
-    imports: [],
-    controllers: [SaveProfitsController, GetProfitsController],
-    providers: [ProfitsRepository],
-    })
+  imports: [],
+  controllers: [SaveProfitsController, GetProfitsController, GetSumProfitsController],
+  providers: [ProfitsRepository],
+})
 
-export class DriverProfitModule {}
+export class DriverProfitModule {};
