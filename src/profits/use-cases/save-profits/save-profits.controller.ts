3c8d4e309bbffb424d5d9ec698b3d5965db4a473
João Henrique <henriquecontact@gmail.com>
2023-11-23 13:19:48 -0300

    Iclude InsertProfitsDTO in save-profits.controller

--- src/profits/use-cases/save-profits/save-profits.controller.ts
+++ src/profits/use-cases/save-profits/save-profits.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { ProfitsRepository } from "../../repositories/profits.repository";
+import { InsertProfitsDTO } from "src/dto/insert-profits.dto";
 
 @Controller('/profits')
 export class SaveProfitsController {
@@ -7,7 +8,7 @@ export class SaveProfitsController {
   constructor (private profitsRepository: ProfitsRepository) {};
   
   @Post('save')
-  insertProfit(@Body() profitValue){
+  insertProfit(@Body() profitValue: InsertProfitsDTO){
     console.log(profitValue);
     this.profitsRepository.salvar(profitValue.Value);
     console.log(profitValue.Value);
