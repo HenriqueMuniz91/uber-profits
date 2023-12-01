cc97e945080341884f620c1c264f5e2ff2450e0c
João Henrique <henriquecontact@gmail.com>
2023-12-01 11:21:42 -0300

    Insert Type information in insert-profits.dto

--- src/dto/insert-profits.dto.ts
+++ src/dto/insert-profits.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, } from "class-validator";
 
 export class InsertProfitsDTO {
-  @IsString()
+  @IsNumber()
   @IsNotEmpty()
   Value: number;
   };
