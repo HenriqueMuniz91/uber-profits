2145d1214f5074f6112e165d5fc643fcff9ab163
João Henrique <henriquecontact@gmail.com>
2023-12-04 02:39:22 -0300

    Create type GetProfitsResponse in get-profits.controller

--- src/profits/use-cases/get-profits/get-profits.controller.ts
+++ src/profits/use-cases/get-profits/get-profits.controller.ts
@@ -1,5 +1,9 @@
 import { Controller, Get } from "@nestjs/common";
 import { ProfitsRepository } from "../../repositories/profits.repository";
+export type GetProfitsResponse = {
+  profit: number
+  date: Date
+}
 
 @Controller ('/profits') 
 export class GetProfitsController {
@@ -7,7 +11,7 @@ export class GetProfitsController {
   constructor (private profitsRepository: ProfitsRepository) {};
   
   @Get('get')
-  getProfits(): number[] {
+  getProfits(): GetProfitsResponse[] {
     const profits = this.profitsRepository.get();
     console.log(profits);
     return profits;
