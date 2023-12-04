b84c11ebb01b382fad237ccdce0bca4637457a1d
João Henrique <henriquecontact@gmail.com>
2023-12-04 02:43:14 -0300

    Chance value for profit.profit in sum-profits.controller

--- src/profits/use-cases/get-sum-profits/get-sum-profits.controller.ts
+++ src/profits/use-cases/get-sum-profits/get-sum-profits.controller.ts
@@ -9,7 +9,7 @@ export class GetSumProfitsController {
   @Get('sum')
   getSumProfits(): number {
     const profits = this.profitsRepository.get();
-    const sum = profits.reduce((accumulator,value) => accumulator + value, 0);
+    const sum = profits.reduce((accumulator,profit) => accumulator + profit.profit, 0);
     return sum;
   };
 };
