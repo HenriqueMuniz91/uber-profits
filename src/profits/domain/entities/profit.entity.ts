0c163ff8879fb6ea4264e1de9d738c24b4819109
João Henrique <henriquecontact@gmail.com>
2023-12-05 11:20:07 -0300

    Create profit.entity.ts

---
+++ src/profits/domain/entities/profit.entity.ts
@@ -0,0 +1,10 @@
+export class Profit {
+  value: number;
+  date: Date;
+  name: string;
+  constructor(value, date, name) {
+    this.value = value;
+    this.date = date;
+    this.name = name;
+  }
+}
