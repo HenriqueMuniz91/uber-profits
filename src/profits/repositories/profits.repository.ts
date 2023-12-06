9a1f264e6007f575782feeef2f2d8c1acd69df1d
João Henrique <henriquecontact@gmail.com>
2023-12-06 03:23:16 -0300

    Create function name in profits.repository.ts

--- src/profits/repositories/profits.repository.ts
+++ src/profits/repositories/profits.repository.ts
@@ -1,12 +1,14 @@
 import { Profit } from "../domain/entities/profit.entity";
-import { GetProfitsResponse } from "../use-cases/get-profits/get-profits.dto";
 
 export class ProfitsRepository {
   private profits: Profit[] = [];
   salvar(profit: Profit): void {
     this.profits.push(profit);
   };
-  get(): Profit[] {
+  get(name?: string): Profit[] {
+    if (name) {
+      return []
+    }
     return this.profits;
   };
 };
